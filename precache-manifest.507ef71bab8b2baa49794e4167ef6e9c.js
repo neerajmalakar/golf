c0cc4882db4ae6c5abd90162714405df930c2fcc
neerajmalakar <malakar.neeraj@gmail.com>
2019-03-15 16:32:11 +0530

    message for work done

---
+++ precache-manifest.507ef71bab8b2baa49794e4167ef6e9c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "923d94c38f92cc94eaef",
+    "url": "/static/css/main.70508c75.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3c4c6ab6b2f48d0a38d7",
+    "url": "/static/js/1.3c4c6ab6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "41022fcc31acda3670656372e5a3fff0",
+    "url": "/static/media/bg.41022fcc.jpg"
+  },
+  {
+    "revision": "891e57734a1ef53b1b54b67fffd918d1",
+    "url": "/static/media/process_bg.891e5773.png"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "923d94c38f92cc94eaef",
+    "url": "/static/js/main.923d94c3.chunk.js"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "3c4c6ab6b2f48d0a38d7",
+    "url": "/static/css/1.b78859a2.chunk.css"
+  },
+  {
+    "revision": "523c789b070f2846156bf66659fb1b2d",
+    "url": "/index.html"
+  }
+];
